dc3194227f2c292929dcdf61520cf13bc9fe9ae2
Mohammed Zarriy <mohammedzarriy1@gmail.com>
2026-04-03 00:14:28 +0100

    Fix adminlte config and add validations menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -16,8 +16,6 @@ return [
     'logo' => '<b>ISMO</b> Archive',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
-    'logo_img_xl' => null,
-    'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'ISMO Logo',
 
     'auth_logo' => [
@@ -25,7 +23,6 @@ return [
         'img' => [
             'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
             'alt' => 'ISMO Logo',
-            'class' => '',
             'width' => 50,
             'height' => 50,
         ],
@@ -43,70 +40,18 @@ return [
         ],
     ],
 
-    'usermenu_enabled' => true,
-    'usermenu_header' => true,
-    'usermenu_header_class' => 'bg-primary',
-    'usermenu_image' => false,
-    'usermenu_desc' => false,
-    'usermenu_profile_url' => false,
-
-    'layout_topnav' => null,
-    'layout_boxed' => null,
     'layout_fixed_sidebar' => true,
     'layout_fixed_navbar' => true,
-    'layout_fixed_footer' => null,
-    'layout_dark_mode' => null,
 
-    'classes_auth_card' => 'card-outline card-primary',
-    'classes_auth_header' => '',
-    'classes_auth_body' => '',
-    'classes_auth_footer' => '',
-    'classes_auth_icon' => '',
-    'classes_auth_btn' => 'btn-flat btn-primary',
-
-    'classes_body' => '',
-    'classes_brand' => '',
-    'classes_brand_text' => '',
-    'classes_content_wrapper' => '',
-    'classes_content_header' => '',
-    'classes_content' => '',
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
-    'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
-    'classes_topnav_nav' => 'navbar-expand',
-    'classes_topnav_container' => 'container',
 
     'sidebar_mini' => 'lg',
-    'sidebar_collapse' => false,
-    'sidebar_collapse_auto_size' => false,
-    'sidebar_collapse_remember' => false,
-    'sidebar_collapse_remember_no_transition' => true,
-    'sidebar_scrollbar_theme' => 'os-theme-light',
-    'sidebar_scrollbar_auto_hide' => 'l',
-    'sidebar_nav_accordion' => true,
-    'sidebar_nav_animation_speed' => 300,
-
-    'right_sidebar' => false,
-    'right_sidebar_icon' => 'fas fa-cogs',
-    'right_sidebar_theme' => 'dark',
-    'right_sidebar_slide' => true,
-    'right_sidebar_push' => true,
-    'right_sidebar_scrollbar_theme' => 'os-theme-light',
-    'right_sidebar_scrollbar_auto_hide' => 'l',
 
     'use_route_url' => true,
     'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
-    'register_url' => false,
-    'password_reset_url' => 'password/reset',
-    'password_email_url' => 'password/email',
-    'profile_url' => false,
-    'disable_darkmode_routes' => false,
-
-    'laravel_asset_bundling' => false,
-    'laravel_css_path' => 'css/app.css',
-    'laravel_js_path' => 'js/app.js',
 
     'menu' => [
 
@@ -121,7 +66,7 @@ return [
             'topnav_right' => true,
         ],
 
-        // Sidebar Search
+        // Sidebar search
         [
             'type' => 'sidebar-menu-search',
             'text' => 'Rechercher',
@@ -131,29 +76,29 @@ return [
         [
             'text' => 'Tableau de bord',
             'url'  => 'dashboard',
-            'icon' => 'fas fa-fw fa-tachometer-alt',
+            'icon' => 'fas fa-tachometer-alt',
         ],
 
         // STAGIAIRES
         ['header' => 'GESTION DES STAGIAIRES'],
         [
             'text' => 'Stagiaires',
-            'icon' => 'fas fa-fw fa-users',
+            'icon' => 'fas fa-users',
             'submenu' => [
                 [
                     'text' => 'Liste des stagiaires',
                     'url'  => 'trainees',
-                    'icon' => 'fas fa-fw fa-list',
+                    'icon' => 'fas fa-list',
                 ],
                 [
-                    'text' => 'Ajouter un stagiaire',
+                    'text' => 'Ajouter',
                     'url'  => 'trainees/create',
-                    'icon' => 'fas fa-fw fa-user-plus',
+                    'icon' => 'fas fa-user-plus',
                 ],
                 [
                     'text' => 'Importer Excel',
                     'url'  => 'trainees/import',
-                    'icon' => 'fas fa-fw fa-file-excel',
+                    'icon' => 'fas fa-file-excel',
                 ],
             ],
         ],
@@ -162,17 +107,16 @@ return [
         ['header' => 'GESTION DES DOCUMENTS'],
         [
             'text' => 'Baccalauréat',
-            'icon' => 'fas fa-fw fa-graduation-cap',
+            'icon' => 'fas fa-graduation-cap',
             'submenu' => [
                 [
                     'text' => 'Liste',
                     'url'  => 'documents/bac',
-                    'icon' => 'fas fa-fw fa-list',
                 ],
                 [
                     'text' => 'Retraits temporaires',
                     'url'  => 'documents/bac/temp-out',
-                    'icon' => 'fas fa-fw fa-clock',
+                    'icon' => 'fas fa-clock',
                     'label' => '!',
                     'label_color' => 'warning',
                 ],
@@ -180,29 +124,28 @@ return [
         ],
         [
             'text' => 'Diplômes',
-            'icon' => 'fas fa-fw fa-certificate',
+            'icon' => 'fas fa-certificate',
             'submenu' => [
                 [
                     'text' => 'Liste',
                     'url'  => 'documents/diplome',
-                    'icon' => 'fas fa-fw fa-list',
                 ],
                 [
-                    'text' => 'Prêts à remettre',
+                    'text' => 'Prêts',
                     'url'  => 'documents/diplome/prets',
-                    'icon' => 'fas fa-fw fa-check-circle',
+                    'icon' => 'fas fa-check-circle',
                 ],
             ],
         ],
         [
-            'text' => 'Bulletins de notes',
+            'text' => 'Bulletins',
             'url'  => 'documents/bulletin',
-            'icon' => 'fas fa-fw fa-file-alt',
+            'icon' => 'fas fa-file-alt',
         ],
         [
             'text' => 'Attestations',
             'url'  => 'documents/attestation',
-            'icon' => 'fas fa-fw fa-file-contract',
+            'icon' => 'fas fa-file-contract',
         ],
 
         // MOUVEMENTS
@@ -210,73 +153,102 @@ return [
         [
             'text' => 'Historique',
             'url'  => 'movements',
-            'icon' => 'fas fa-fw fa-exchange-alt',
+            'icon' => 'fas fa-exchange-alt',
         ],
         [
-            'text' => 'Retraits du jour',
+            'text' => 'Aujourd’hui',
             'url'  => 'movements/today',
-            'icon' => 'fas fa-fw fa-calendar-day',
+            'icon' => 'fas fa-calendar-day',
         ],
 
-        // VALIDATIONS (✔ ajouté)
+        // VALIDATIONS
         ['header' => 'VALIDATIONS'],
         [
-            'text' => 'Registre des validations',
+            'text' => 'Registre',
             'url'  => 'validations',
-            'icon' => 'fas fa-fw fa-check-double',
+            'icon' => 'fas fa-check-double',
         ],
 
-        // ADMINISTRATION
+        // ADMIN
         ['header' => 'ADMINISTRATION'],
         [
             'text' => 'Utilisateurs',
             'url'  => 'users',
-            'icon' => 'fas fa-fw fa-user-cog',
-            'can'  => 'manage-users',
+            'icon' => 'fas fa-user-cog',
         ],
-        [
-            'text' => 'Paramètres',
-            'icon' => 'fas fa-fw fa-sitemap',
-            'can'  => 'manage-users',
-            'submenu' => [
+    ],
+
+    'plugins' => [
+
+        'Datatables' => [
+            'active' => true,
+            'files' => [
                 [
-                    'text' => 'Secteurs',
-                    'url'  => 'secteurs',
-                    'icon' => 'fas fa-fw fa-building',
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
                 ],
                 [
-                    'text' => 'Filières',
-                    'url'  => 'filieres',
-                    'icon' => 'fas fa-fw fa-code-branch',
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                ],
+            ],
+        ],
+
+        'Select2' => [
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
+                ],
+            ],
+        ],
+
+        'Sweetalert2' => [
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@11',
+                ],
+            ],
+        ],
+
+        'Chartjs' => [
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
+                ],
+            ],
+        ],
+
+        'Pace' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
                 ],
             ],
         ],
     ],
 
-    'filters' => [
-        JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
-    ],
-
-    'plugins' => [
-        'Datatables' => ['active' => true],
-        'Select2' => ['active' => true],
-        'Sweetalert2' => ['active' => true],
-        'Chartjs' => ['active' => true],
-        'Pace' => ['active' => false],
-    ],
-
-    'iframe' => [
-        'default_tab' => [
-            'url' => null,
-            'title' => null,
-        ],
-    ],
-
-    'livewire' => false,
 ];
